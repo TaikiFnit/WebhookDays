9f1209c2cc27fe9cf9c5190bb2570f8e505843e5
TaikiFnit <taikifnitspg@gmail.com>
2018-01-23 21:30:20 +0900

    payload fix

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const day = now.getDate()
 
 const couchdb_url = "http://fnit.site:5984/adler-words/" + month
 const discord_host = "discordapp.com"
-const discord_path = "/api/webhooks/"
+const discord_path = "/api/webhooks/403393085364764672/C-BW2CWmAiJ6SQN_LeS_UZlyCZXXERrRfM40eL_vKth7QTNPM4JMP3nDRwp3UPwBXnAc"
 
 http.get(couchdb_url, (res) => {
     res.setEncoding('utf8')
@@ -32,10 +32,8 @@ http.get(couchdb_url, (res) => {
         }
         
         let todays_word = words_document["days"][day]
-        // let heading = todays_word["heading"]
-        let heading = "見出し"
-        // let paragraph = todays_word["paragraph"]
-        let paragraph = "コンテント"
+        let heading = todays_word["heading"]
+        let paragraph = todays_word["paragraph"]
 
         let content = `\`\`\`\n${heading}\n${paragraph}\n\`\`\``
 
